3f855d69110d7cb8b459ea3aadc2995de653a794
tangqiang <3256224359@qq.com>
2022-08-14 22:30:36 +0800

    配置hutool依赖 配置axios依赖 配置elementui依赖并导入 配置storejs 配置mainjs前端拦截器

--- vue2demo1/src/main.js
+++ vue2demo1/src/main.js
@@ -2,11 +2,63 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+
+Vue.use(ElementUI);
 
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requireAuth) {
+    console.log("前端拦截器启动");
+    // 模板
+    // if(to.path.indexOf('')){
+    //   if (store.state.){
+    //     next();
+    //   }else{
+    //     alert("请登录账号");
+    //     next('/');
+    //   }
+    // }
+    if(to.path.indexOf('/customer')==0){
+      if (store.state.customer.cid){
+        next();
+      }else{
+        alert("请登录账号");
+        next('/');
+      }
+    }
+
+    if(to.path.indexOf('/storekeeper')==0){
+      if (store.state.storeKeeper.sid){
+        next();
+      }else{
+        alert("请登录账号");
+        next('/');
+      }
+    }
+    // console.log(to.path.match(new RegExp("^/storekeeper")));
+    // console.log(to.path=='/storekeeper');
+    // console.log(this.$route.query);
+    // next();
+    // if (store.state.customer.cname) {
+    //   console.log("welcome"+store.state.customer.cname)
+    //   next()
+    // } else {
+    //   // 跳到登录
+    //   alert("拦截")
+    //   next({path: '/', query: {redirect: to.fullPath}})
+    // }
+  } else {
+    next()
+  }
+})
 
 new Vue({
   router,
   store,
   render: h => h(App)
 }).$mount('#app')
+
+
--- vue2demo1/src/store/index.js
+++ vue2demo1/src/store/index.js
@@ -5,10 +5,52 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    customer: {
+      cid:
+          window.localStorage
+              .getItem('customerinfo' || '[]') == null ? 0 : JSON.parse(window.localStorage.getItem('customerinfo' || '[]'))
+              .cid,
+      cname:
+          window.localStorage
+              .getItem('customerinfo' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('customerinfo' || '[]'))
+              .cname,
+      cpassword:
+          window.localStorage
+              .getItem('customerinfo' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('customerinfo' || '[]'))
+              .cpassword
+
+    },
+    storeKeeper: {
+      sid:
+          window.localStorage
+              .getItem('storekeeperinfo' || '[]') == null ? 0 : JSON.parse(window.localStorage.getItem('storekeeperinfo' || '[]'))
+              .sid,
+      sname:
+          window.localStorage
+              .getItem('storekeeperinfo' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('storekeeperinfo' || '[]'))
+              .sname,
+      spassword:
+          window.localStorage
+              .getItem('storekeeperinfo' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('storekeeperinfo' || '[]'))
+              .spassword,
+      isbank:
+          window.localStorage
+              .getItem('storekeeperinfo' || '[]') == null ? '' : JSON.parse(window.localStorage.getItem('storekeeperinfo' || '[]'))
+              .isbank
+
+    }
   },
   getters: {
   },
   mutations: {
+    saveCustomer(state, customer) {
+      state.customer = customer;
+      window.localStorage.setItem('customerinfo', JSON.stringify(customer))
+    },
+    saveStoreKeeper(state, storeKeeper) {
+      state.storeKeeper = storeKeeper;
+      window.localStorage.setItem('storekeeperinfo', JSON.stringify(storeKeeper))
+    }
   },
   actions: {
   },
